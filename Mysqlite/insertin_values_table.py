74ce72ddddc248f83b7fe6b3b8bee07b6c3d15ea
sahil_1123 <59478266+Sahil123git@users.noreply.github.com>
2021-11-28 22:32:09 +0530

    Update insertin_values_table.py

--- Mysqlite/insertin_values_table.py
+++ Mysqlite/insertin_values_table.py
@@ -2,7 +2,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "Simba@123",
+    password = "fgdfgsgfds",
     database = "t20wc"
     )
 m = db.cursor()
